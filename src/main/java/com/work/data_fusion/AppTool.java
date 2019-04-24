d972edb72852067e6b6b936e89a47f0bd1eb5e51
haibo.wang <haibo.wang@yulore.com>
2019-04-24 17:34:59 +0800

    相关组件使用SPI加载

--- src/main/java/com/work/data_fusion/AppTool.java
+++ src/main/java/com/work/data_fusion/AppTool.java
@@ -1,5 +1,9 @@
 package com.work.data_fusion;
 
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import com.work.data_fusion.exception.BusinessException;
 import com.work.data_fusion.fusion.DataFusion;
 import com.work.data_fusion.loader.DataGroupLoader;
 import com.work.data_fusion.loader.DataLoader;
@@ -9,15 +13,56 @@ import com.work.data_fusion.storage.DataGroupStorage;
 
 public class AppTool {
 	public boolean run(String[] args){
+		return loadFusionService().fuse();
+	}
+	
+	private FusionService loadFusionService(){
 		DataGroupLoader dataGroupLoader = null;
+		ServiceLoader<DataGroupLoader> dataServiceLoaderService = ServiceLoader.load(DataGroupLoader.class);
+		Iterator<DataGroupLoader> dglItr = dataServiceLoaderService.iterator();
+		while(dglItr.hasNext()){
+			dataGroupLoader = dglItr.next();
+		}
+		if(null==dataGroupLoader){
+			throw new BusinessException("Not found DataGroupLoader");
+		}
+		
 		DataLoader dataLoader = null;
+		ServiceLoader<DataLoader> dataLoaderService = ServiceLoader.load(DataLoader.class);
+		Iterator<DataLoader> dlItr = dataLoaderService.iterator();
+		while(dlItr.hasNext()){
+			dataLoader = dlItr.next();
+		}
+		if(null==dataLoader){
+			throw new BusinessException("Not found DataLoader");
+		}
+		
 		DataFusion dataFusion = null;
+		ServiceLoader<DataFusion> dataFuseService = ServiceLoader.load(DataFusion.class);
+		Iterator<DataFusion> dfItr = dataFuseService.iterator();
+		while(dfItr.hasNext()){
+			dataFusion = dfItr.next();
+		}
+		if(null==dataFusion){
+			throw new BusinessException("Not found DataFusion");
+		}
+		
 		DataGroupStorage dataGroupStorage = null;
+		ServiceLoader<DataGroupStorage> dataGroupStorageService = ServiceLoader.load(DataGroupStorage.class);
+		Iterator<DataGroupStorage> dataGroupStorageItr = dataGroupStorageService.iterator();
+		while(dataGroupStorageItr.hasNext()){
+			dataGroupStorage = dataGroupStorageItr.next();
+		}
+		if(null==dataGroupStorage){
+			throw new BusinessException("Not found DataGroupStorage");
+		}
+		
 		FusionService fusionService = FusionServiceBuilder.create()
 				.setDataGroupLoader(dataGroupLoader)
 				.setDataLoader(dataLoader)
 				.setDataFusion(dataFusion)
 				.setDataGroupStorage(dataGroupStorage).build();
-		return fusionService.fuse();
+		return fusionService;
 	}
+	
 }
--- src/main/java/com/work/data_fusion/exception/BusinessException.java
+++ src/main/java/com/work/data_fusion/exception/BusinessException.java
@@ -1,8 +1,37 @@
 package com.work.data_fusion.exception;
 
 public class BusinessException extends RuntimeException{
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 7473477575077091203L;
+
+	public BusinessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusinessException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusinessException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusinessException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusinessException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
