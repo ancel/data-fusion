cc76938b4b180f0ea718c4ef08793b685f2f0da2
haibo.wang <haibo.wang@yulore.com>
2020-03-30 10:46:39 +0800

    update

--- src/main/java/com/work/data_fusion/App.java
+++ src/main/java/com/work/data_fusion/App.java
@@ -5,7 +5,6 @@ public class App {
 	static {
 		System.setProperty("user.timezone","GMT+8");
 		System.setProperty("log4j.configurationFile", "conf/log4j2.xml");
-		System.setProperty("com.mchange.v2.c3p0.cfg.xml", "conf/c3p0-config.xml");
     }
 	public static void main(String[] args) {
 		boolean result = new AppTool().run(args);
